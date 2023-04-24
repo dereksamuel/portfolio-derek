65b38d0062c296fd24d28604815d2099f39828a4
Derek Paul <dereksamuelgr@gmail.com>
2023-04-24 15:38:23 -0500

    done cursor megaman

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import Button from '@/components/Atoms/Button'
+import Cursor from '@/components/Atoms/Cursor'
 
 export default function Home () {
   return (
     <>
+      <Cursor />
       <h1><strong>[Subject]: Derek Paul</strong></h1>
-      <Button theme='primary'>Derek Samuel Paul Pena</Button>
       <Button theme='primary'>Portfolio</Button>
-      <Button theme='light'>About me</Button>
-      <Button theme='secondary'>Disabled</Button>
-      <Button theme='danger'>Danger</Button>
-      <Button theme='success'>Danger</Button>
-      <Button theme='warning'>Danger</Button>
     </>
   )
 }
